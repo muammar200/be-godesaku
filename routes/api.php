e39c2eced45b5eee0921d3ef807d52d23b0e5968
muammar200 <amuammar01@gmail.com>
2024-10-04 19:19:11 +0800

    tes api listing

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,66 @@ Route::prefix('public')->group(function () {
 
         return response()->json($data);
     });
+    Route::get('/listing-locations', function () {
+        $locations = [
+            'status' => true,
+            'message' => 'Show All Listing Success',
+            'data' => [
+                    [
+                        "id" => 1,
+                        "name" => "Sultan Ayam Geprek Samata",
+                        "location" => "Samata, Gowa",
+                        "latitude" => -5.20218,
+                        "longitude" => 119.49572
+                    ],
+                    [
+                        "id" => 2,
+                        "name" => "Bokatana Samata",
+                        "location" => "Samata, Gowa",
+                        "latitude" => -5.20196,
+                        "longitude" => 119.49466
+                    ],
+                    [
+                        "id" => 3,
+                        "name" => "Bukit Samata",
+                        "location" => "Samata, Gowa",
+                        "latitude" => -5.20083,
+                        "longitude" => 119.49733
+                    ],
+                    [
+                        "id" => 4,
+                        "name" => "UPT Perpustakaan Syekh Yusuf UIN Alauddin Makassar",
+                        "location" => "Samata, Gowa",
+                        "latitude" => -5.20667,
+                        "longitude" => 119.49751
+                    ],
+                    [
+                        "id" => 5,
+                        "name" => "Puskesmas Samata",
+                        "location" => "Samata, Gowa",
+                        "latitude" => -5.20103,
+                        "longitude" => 119.48786
+                    ]
+                ]
+        ];
+
+        // if (isset($locations['data'][$id])) {
+        //     $response = [
+        //         'status' => true,
+        //         'message' => $locations['message'],
+        //         'data' => $locations['data'][$id]
+        //     ];
+        // } else {
+        //     return response()->json([
+        //         'status' => false,
+        //         'message' => 'Location not found',
+        //         'data' => []
+        //     ], 404); 
+        // }
+
+        return response()->json($locations);
+
+    });
     Route::get('/listing-locations/{id}', function ($id) {
         $locations = [
             'status' => true,
@@ -41,12 +101,14 @@ Route::prefix('public')->group(function () {
                     [
                         "id" => 1,
                         "name" => "Sultan Ayam Geprek Samata",
+                        "location" => "Samata, Gowa",
                         "latitude" => -5.20218,
                         "longitude" => 119.49572
                     ],
                     [
                         "id" => 2,
                         "name" => "Bokatana Samata",
+                        "location" => "Samata, Gowa",
                         "latitude" => -5.20196,
                         "longitude" => 119.49466
                     ]
@@ -55,6 +117,7 @@ Route::prefix('public')->group(function () {
                     [
                         "id" => 3,
                         "name" => "Bukit Samata",
+                        "location" => "Samata, Gowa",
                         "latitude" => -5.20083,
                         "longitude" => 119.49733
                     ],
@@ -63,12 +126,14 @@ Route::prefix('public')->group(function () {
                     [
                         "id" => 4,
                         "name" => "UPT Perpustakaan Syekh Yusuf UIN Alauddin Makassar",
+                        "location" => "Samata, Gowa",
                         "latitude" => -5.20667,
                         "longitude" => 119.49751
                     ],
                     [
                         "id" => 5,
                         "name" => "Puskesmas Samata",
+                        "location" => "Samata, Gowa",
                         "latitude" => -5.20103,
                         "longitude" => 119.48786
                     ]
@@ -94,6 +159,21 @@ Route::prefix('public')->group(function () {
 
     });
 
+    // Route::prefix('teschart')->group(function(){
+    //     Route::get('/jumlah-penduduk', [API\PendudukController::class, 'getPendudukByAgeCategoryEachYear']);
+    //     Route::get('/jenis-kelamin', function(){
+            
+    //     });
+    // });
+    Route::get('/populations/generals', [API\Public\PopulationController::class, 'countGenerals']);
+    Route::get('/populations/genders', [API\Public\PopulationController::class, 'countGenders']);
+    Route::get('/populations/civics', [API\Public\PopulationController::class, 'countCivics']);
+    Route::get('/populations/religions', [API\Public\PopulationController::class, 'countReligions']);
+    Route::get('/populations/educations', [API\Public\PopulationController::class, 'countEducations']);
+    Route::get('/populations/professions', [API\Public\PopulationController::class, 'countProfessions']);
+    Route::get('/populations/marriages', [API\Public\PopulationController::class, 'countMarriageStatuses']);
+
+    Route::get('/news', [API\Public\NewsController::class, 'index']);
     Route::get('/news', [API\Public\NewsController::class, 'index']);
     Route::get('/news/{news}', [API\Public\NewsController::class, 'show']);
     Route::get('/travel-articles', [API\Public\TravelArticleController::class, 'index']);
