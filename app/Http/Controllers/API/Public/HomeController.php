d7f2d9007662c0150d25fbd7d94ad4f5c9308518
muammar200 <amuammar01@gmail.com>
2024-10-18 09:20:21 +0800

    feat(public): add title to profile summary response

--- app/Http/Controllers/API/Public/HomeController.php
+++ app/Http/Controllers/API/Public/HomeController.php
@@ -6,11 +6,12 @@ use App\Models\Slider;
 use App\Models\Contact;
 use App\Models\SocialMedia;
 use Illuminate\Http\Request;
+use App\Models\MasterPopulation;
+use App\Models\GeneralInformation;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Public\SliderResource;
 use App\Http\Resources\Public\ContactResource;
 use App\Http\Resources\Public\SocialMediaResource;
-use App\Models\GeneralInformation;
 
 class HomeController extends Controller
 {
@@ -58,6 +59,7 @@ class HomeController extends Controller
             'status' => true,
             'message' => 'Berhasil Menampilkan Profil Singkat Desa',
             'data' => [
+                'title' => 'Profil singkat ' . GeneralInformation::where('id', 1)->value('village_name'),
                 'deskripsi' => $profile_summary
             ],
         ];
@@ -180,5 +182,20 @@ class HomeController extends Controller
         ];
 
         return response()->json($data, 200);   
+    }
+
+    public function showVillageInformation()
+    {
+        $count_population = MasterPopulation::count();
+
+        $data = [
+            'status' => true,
+            'message' => 'Menampilkan Informasi Desa',
+            'data' => [
+                'Jumlah Penduduk' => $count_population,
+            ],
+        ];
+
+        return response()->json($data, 200);
     }
 }
