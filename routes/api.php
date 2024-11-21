2737ed6fb69c4a6f50d286422bb43ad22e3bfbcd
muammar200 <amuammar01@gmail.com>
2024-11-21 15:20:02 +0800

    feat(public/apb): add api chart pendapatan dan belanja

--- routes/api.php
+++ routes/api.php
@@ -224,6 +224,8 @@ Route::prefix('public')->group(function () {
     
     Route::get('/years', [API\Public\ApbDesaController::class, 'getLastFiveYears']);
     Route::get('/apb-desa/generals', [API\Public\ApbDesaController::class, 'index']);
+    Route::get('/apb-desa/revenues-and-expenses/title', [API\Public\ApbDesaController::class, 'titleRevenueAndExpense']);
+    Route::get('/apb-desa/revenues-and-expenses', [API\Public\ApbDesaController::class, 'countRevenueAndExpense']);
     Route::get('/apb-desa/revenues/title', [API\Public\ApbDesaController::class, 'titleRevenue']);
     Route::get('/apb-desa/revenues', [API\Public\ApbDesaController::class, 'countRevenue']);
     Route::get('/apb-desa/revenues/list', [API\Public\ApbDesaController::class, 'getRevenue']);
